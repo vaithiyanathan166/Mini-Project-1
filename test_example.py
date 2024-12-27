93e9e832fccc598239111298d834b09300b7e8ac
vaithiyanathan <vaithies98@gmail.com>
2024-12-27 17:44:24 +0530

    test_example.py

--- test_example.py
+++ test_example.py
@@ -1,5 +1,5 @@
-# def test_addition():
-#     assert 1 + 1 == 2
+ def test_addition():
+    assert 1 + 1 == 2
 
-# def test_subtraction():
-#     assert 5 - 3 == 2
+ def test_subtraction():
+    assert 5 - 3 == 2
